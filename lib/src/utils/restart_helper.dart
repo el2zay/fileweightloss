a98741bd9b930f0be753f40abf51f889a7499014
Elie <79168733+el2zay@users.noreply.github.com>
2025-06-27 21:44:50 +0200

    Fix : Restart sur windows

--- lib/src/utils/restart_helper.dart
+++ lib/src/utils/restart_helper.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:window_manager/window_manager.dart';
 
 class RestartHelper {
   static Future<void> restartApp() async {
-    if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
-      final executable = Platform.resolvedExecutable;
+    final executable = Platform.resolvedExecutable;
 
-      await Process.start(executable, [], mode: ProcessStartMode.detached);
-
+    await Process.start(executable, [], mode: ProcessStartMode.detached);
+    if (Platform.isMacOS) {
       SystemNavigator.pop();
+    } else {
+      await windowManager.close();
     }
   }
 }
